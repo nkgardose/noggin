a884836d2067c8cc8cf2add4405b822f2d170311
Neilson Gardose <neilson.keith.gardose@gmail.com>
2023-04-26 04:47:37 +0800

    feat: implement hook and helpers for players feat

---
+++ src/utils/keyboardCommands.ts
@@ -0,0 +1,9 @@
+export enum KeyboardEvents {
+  R = 'r', // roll
+  ArrowUp = 'ArrowUp', // go up player
+  ArrowDown = 'ArrowDown', // go down player
+  ArrowRight = 'ArrowRight', // increment point for a player
+  ArrowLeft = 'ArrowLeft', // decrement point for a player
+  Backspace = 'Backspace', // delete a player
+  Enter = 'Enter', // add a player
+}
